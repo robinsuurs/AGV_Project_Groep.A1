351b5f5d62cecaad9891549b3b0e5aad3d984382
Robin Suurs <17059143@student.hhs.nl>
2022-05-27 15:03:14 +0200

    timmer setup voor 1 motor
    gebruikt timmer0 met een snelheid van clk/256

--- AGV_Code/main.c
+++ AGV_Code/main.c
@@ -9,6 +9,18 @@
 #define ClearBit(byte, bit)     (byte &= ~BV(bit))
 #define ToggleBit(byte, bit)    (byte ^= BV(bit))
 
+void init_timer(void)
+{
+    ///timmer settup voor stepper motor
+    ///gebruikt timer0 in CTC mode
+    ///met een frequentie tussen 2 en 8 microSeconden
+    TCCR0A |= BV(COM0A0) | BV(WGM01);
+    TCCR0B |= BV(CS02);
+    OCR0A = 255; //max frequentie als start
+    SetBit(DDRD, PD6); //enable output timer
+
+}
+
 
 int main(void)
 {
