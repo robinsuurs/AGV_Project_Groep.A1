ec6b2a010041e893c1ffc4b331efe2ba7da9a6e1
patrickh33msk3rk <105854840+patrickh33msk3rk@users.noreply.github.com>
2022-06-21 17:19:20 +0200

    AGV_V3_compleet
    verglijken met beginwaarden een corrigeren aan de hand van berekening

--- AGV_Code/main.c
+++ AGV_Code/main.c
@@ -8,40 +8,31 @@
 #define SetBit(byte, bit)       (byte |= BV(bit))
 #define ClearBit(byte, bit)     (byte &= ~BV(bit))
 #define ToggleBit(byte, bit)    (byte ^= BV(bit))
+#define SetBit(byte, bit)       (byte |= BV(bit))
+#define ClearBit(byte, bit)     (byte &= ~BV(bit))
+#define ToggleBit(byte, bit)    (byte ^= BV(bit))
 
-volatile uint16_t ADC_waarde[3];
-volatile int8_t ADC_pin;
-
-ISR(ADC_vect)
-{
-    ADC_waarde[ADC_pin] = ADC;
-    for(ADC_pin; ADC_pin < 3; ++ADC_pin)
-    {
-        ADMUX = (0XF0 & ADMUX) | ADC_pin;
-        SetBit(ADCSRA, ADSC);
-        break;
-    }
-}
-
-double MapRange(double X, double A1, double A2, double B1, double B2)
-{
-    double Y = B1 + (X-A1)*((B2-B1)/(A2-A1));
-    return Y;
-}
+volatile uint16_t ADC_waarde;
+volatile uint16_t POT0_BEGIN;
+volatile uint16_t POT2_BEGIN;
+volatile uint16_t verschil_0;
+volatile uint16_t verschil_2;
+volatile uint16_t ADC_waarde_0;
+volatile uint16_t ADC_waarde_2;
 
 void init_timer(void)
 {
-    ///timmer settup voor stepper motor1
-    ///gebruikt timer0 in CTC mode
-    ///met snelheid tussen 70 als langzaamste en 15 als snelste
+    //timmer settup voor stepper motor1
+    //gebruikt timer0 in CTC mode
+    //met snelheid tussen 70 als langzaamste en 15 als snelste
     TCCR0A  = BV(COM0A0) | BV(WGM01);
     TCCR0B  = BV(CS02);             // clk/256
     OCR0A   = 15;                   //start snelheid
     SetBit(DDRD, PD6);              //enable output timer D6
 
-    ///timmer settup voor stepper motor2
-    ///gebruikt timer2 in CTC mode
-    ///met snelheid tussen 70 als langzaamste en 15 als snelste
+    //timmer settup voor stepper motor2
+    //gebruikt timer2 in CTC mode
+    //met snelheid tussen 70 als langzaamste en 15 als snelste
     TCCR2A  = BV(COM2A0) | BV(WGM21);
     ClearBit(ASSR, AS2);             // interne io klok geselecteerd
     TCCR2B  = BV(CS22) | BV(CS21);   // clk/256
@@ -51,36 +42,85 @@ void init_timer(void)
 
 void init_adc(void)
 {
-    ADMUX |= (1 << REFS0);                                  // AVCC als referentiespanning
-    ADCSRA = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);    // Prescaler clk/128 = 125kHZ
-    ADCSRA |= (1 << ADEN) | BV(ADIE);                       // Aanzetten ADC
+    ADCSRA = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);    // Prescaler 128 125kHZ
+    ADCSRA |= (1 << ADEN);                                  // Aanzetten ADC
+
 
     // Waarde 1 genereren
     ADMUX &= ~(1 << MUX0);                          // ADC op poort 0
     ADCSRA |= (1 << ADSC);                          // Conversatie starten
+    while (ADCSRA & (1 << ADSC));{}                 // Wacht tot conversatie klaar is
+    POT0_BEGIN = ADC;                               // Waarde meegeven aan variabele
+
+
+    // Waarde 2 genereren
+    ADMUX |= (1 << MUX0);                           // ADC op poort 1
+    ADCSRA |= (1 << ADSC);                          // Conversatie starten
+    while (ADCSRA & (1 << ADSC));{}                 // Wacht tot conversatie klaar is
+    POT2_BEGIN = ADC;                             // Waarde meegeven aan variabele
+
+
+
 }
 
+void ADC_Check(void)
+{
+    ADMUX &= ~(1 << MUX0);                          // ADC op poort 0
+    ADCSRA |= (1 << ADSC);                          // Conversatie starten
+    while (ADCSRA & (1 << ADSC));{}                 // Wacht tot conversatie klaar is
+    ADC_waarde_0 = ADC;                             // Waarde meegeven aan variabele
+
+    // Waarde 2 genereren
+    ADMUX |= (1 << MUX0);                           // ADC op poort 1
+    ADCSRA |= (1 << ADSC);                          // Conversatie starten
+    while (ADCSRA & (1 << ADSC));{}                 // Wacht tot conversatie klaar is
+    ADC_waarde_2 = ADC;                             // Waarde meegeven aan variabele
+
+    if(POT0_BEGIN < ADC_waarde_0)
+    {
+        verschil_0 = ADC_waarde_0 - POT0_BEGIN;
+    }
+    if(POT0_BEGIN > ADC_waarde_0)
+    {
+        verschil_0 = POT0_BEGIN - ADC_waarde_0;
+        if(verschil_0 < 150)
+        {
+            OCR0A = 30 ;
+            OCR2A = 30 ;
+        }
+        if(verschil_0 > 150)
+        {
+            OCR0A = 40 ;
+            OCR2A = 20 ;
+        }
+    }
+    if(POT2_BEGIN < ADC_waarde_2)
+    {
+        verschil_2 = ADC_waarde_2 - POT2_BEGIN;
+        if(verschil_2 < 150)
+        {
+            OCR0A = 30 ;
+            OCR2A = 30 ;
+        }
+        if(verschil_2 > 150)
+        {
+            OCR0A = 20 ;
+            OCR2A = 40 ;
+        }
+    }
+}
 
 int main(void)
 {
     init_timer();
     init_adc();
-    // Insert code
+
 
     while(1)
     {
-        if(ADC_waarde[0] <= ADC_waarde[1])
-        {
-            OCR0A = 70;     //slow speed
-            OCR2A = 15;     //full speed
-        }
-        else if(ADC_waarde[0] > ADC_waarde[1])
-        {
-            OCR2A = 70;     //slowspeed
-            OCR0A = 15;     //fullspeed
-        }
+        ADC_Check();
     }
-
 
     return 0;
 }
+
